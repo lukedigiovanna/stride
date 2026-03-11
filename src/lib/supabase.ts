983e0fbf2e7c429a28eba25b077e5f500a5fba7b
lukedigiovanna <lukedigiovanna@gmail.com>
2026-03-11 01:42:51 -0400

    dangerously insert env vars

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = "https://ygxetvlkqwbsuyxlmkuf.supabase.co" as string;
+const supabaseAnonKey = "sb_publishable_tMCmN_v-WBjjMHz1vXlFpw_RAGbQTPI" as string;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
